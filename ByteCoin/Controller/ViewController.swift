3fbfe30c90b89ab10de030bf049da7f06856fe48
MarvinBlanca-LIGPH <76429291+MarvinBlanca-LIGPH@users.noreply.github.com>
2021-03-09 11:37:01 +0800

    Challenge done

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -8,13 +8,62 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController{
+    
+    @IBOutlet weak var bitcoinLabel: UILabel!
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
+    var coinManager = CoinManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        currencyPicker.dataSource = self
+        currencyPicker.delegate = self
+        coinManager.delegate = self
+        
+        coinManager.getCoinPrice(for: "AUD")
     }
-
-
 }
 
+//MARK: - PickerViewDataSource
+
+extension ViewController: UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count
+    }
+}
+
+//MARK: - PickerViewDelegate
+
+extension ViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selectedCurrency = coinManager.currencyArray[row]
+        coinManager.getCoinPrice(for: selectedCurrency)
+    }
+}
+
+//MARK: - CoinManagerDelegate
+
+extension ViewController: CoinManagerDelegate {
+    func didUpdateRate(_ bitcoinRate: BitcoinRateData) {
+        DispatchQueue.main.async {
+            self.bitcoinLabel.text = String(format: "%.2f",bitcoinRate.rate)
+            self.currencyLabel.text = bitcoinRate.asset_id_quote
+        }
+    }
+    
+    func didFailWithError(_ error: Error) {
+        print(error)
+    }
+}
--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -8,12 +8,56 @@
 
 import Foundation
 
+protocol CoinManagerDelegate {
+    func didUpdateRate(_ bitcoinRate: BitcoinRateData)
+    func didFailWithError(_ error: Error)
+}
 struct CoinManager {
-    
+    var delegate: CoinManagerDelegate?
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let apiKey = "825D8232-4FAE-4815-B841-03163087EE36"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
-
     
+    func getCoinPrice(for currency: String) {
+        let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
+        performReqest(urlString)
+    }
+    
+    func performReqest(_ urlString: String) {
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    self.delegate?.didFailWithError(error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    if let bitcoinRate = self.parseJSON(safeData) {
+                        self.delegate?.didUpdateRate(bitcoinRate)
+                    }
+                }
+            }
+            
+            task.resume()
+        }
+    }
+    
+    func parseJSON(_ weatherData: Data) -> BitcoinRateData? {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(BitcoinRateData.self, from: weatherData)
+            let id = decodedData.asset_id_base
+            let currency = decodedData.asset_id_quote
+            let rate = decodedData.rate
+            
+            let bitcoinRate = BitcoinRateData(asset_id_base: id, asset_id_quote: currency, rate: rate)
+            return bitcoinRate
+        } catch {
+            self.delegate?.didFailWithError(error)
+            return nil
+        }
+    }
+
 }
